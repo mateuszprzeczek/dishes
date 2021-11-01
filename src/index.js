4a9165944ce7905c3599acd5ac9f259e2c4684dc
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-01 11:04:15 +0100

    required dependencies added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import sendOrder from './sendOrder'
+import DishesForm from './components/DishesForm.js';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <DishesForm onSubmit={sendOrder}/>
+  </Provider>  
+ ,
+  document.getElementById('root')
+);
