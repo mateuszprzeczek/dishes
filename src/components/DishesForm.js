5bfbb687d9a10bce6ea99bf42b716c494ac9eeb7
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-02 11:11:21 +0100

    refactoring DishForm, removing dead code

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -2,68 +2,69 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm, formValueSelector  } from 'redux-form';
 import Pizza from './dish-types/Pizza';
+import Soup from './dish-types/Soup';
+import Sandwith from './dish-types/Sandwith';
 import renderField from '../renderField';
 import validate from '../validate';
-
-
 
 
 let DishesForm = props => {
     const { handleSubmit,
         dishType,
-        pristine, reset, submitting
+        pristine, submitting
        } = props;
 
-       if(dishType) {
-         console.log(dishType)
-       }
-
-
-
     return <form onSubmit={handleSubmit}>
+        
         <div>
-        <div>
-          <Field name="name" component={renderField} type="text" label="Name"/>
+          <Field 
+          name="name" 
+          component={renderField} 
+          type="text" 
+          label="Name"/>
         </div>
-      </div>
-      <div>
+    
+     
         <div>
-          <Field name="preparation_time" component={renderField} type="time" step="2" defaultValue="00:00:00" label="Preparation time"/>
+          <Field 
+          name="preparation_time" 
+          component={renderField} 
+          type="time" 
+          step="1" 
+          defaultValue="00:00:00" 
+          label="Preparation time"/>
         </div>
-      </div>
+      
       <div>
         <label>Dish type</label>
         <div>
-          <Field name="type" component="select">
-            <option></option>
+          <Field 
+          name="type" 
+          component="select">
+            <option>Select dish type</option>
             <option name="pizza" value="pizza">pizza</option>
             <option value="soup">soup</option>
             <option value="sandwith">sandwith</option>
           </Field>
         </div>
-        {(dishType === 'pizza') &&
-               <Pizza />
-               }
+        {(dishType === 'pizza') && <Pizza />}
+        {(dishType === 'soup') && <Soup />}
+        {(dishType === 'sandwith') && <Sandwith />}
       </div>
       <div>
         <button type="submit" disabled={pristine || submitting}>Submit</button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values
-        </button>
       </div>
       </form>
   }
   
   DishesForm = reduxForm({    
     form: 'dishesForm',
-    destroyOnUnmount: false, 
-    forceUnregisterOnUnmount: true, 
     validate
   })(DishesForm)
 
 const selector = formValueSelector('dishesForm') 
 DishesForm = connect(state => {
-  const dishType = selector(state, 'type');
-  
+  const dishType = selector(state, 'type'); 
   return {
     dishType,
   }
