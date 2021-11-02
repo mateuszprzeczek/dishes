c78accf633c7c2f4743064b433fc4a611508f8b1
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-02 11:13:19 +0100

    added max value for render field function

--- src/renderField.js
+++ src/renderField.js
@@ -1,9 +1,9 @@
-const renderField = ({ input, label, type, step, min, value, defaultValue, meta: { touched, error, warning } }) => (
+const renderField = ({ input, label, type, step, min, max, value, defaultValue, meta: { touched, error } }) => (
   <div>
     <label>{label}</label>
     <div>
-      <input {...input} placeholder={label} step={step} min={min} value={value} defaultValue={defaultValue} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      <input {...input} placeholder={label} step={step} min={min} max={max} value={value} defaultValue={defaultValue} type={type}/>
+      {touched && (error && <span>{error}</span>)}
     </div>
   </div>
 )
