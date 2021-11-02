9a56c67e4d2d7cc9726a1177fcb83f3805b42a9f
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-02 11:07:06 +0100

    Soup component for dish select option was created

---
+++ src/components/dish-types/Soup.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Field } from 'redux-form';
+import renderField from '../../renderField';
+
+let Soup = () => {
+    return (
+        <div>
+            <Field
+                className="slider"
+                name="spiciness_scale"
+                id="spiciness_scale"
+                component={renderField}
+                type="range"
+                min="1"
+                max="10"
+                value=""
+                defaultValue="-1"
+                label="spiciness scale 1-10"
+            />
+        </div>       
+    )
+}
+
+
+export default Soup;
