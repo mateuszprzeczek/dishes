e07fc238044391572fc0f991e44975c37a03a876
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-02 18:57:34 +0100

    directory for DishesForm was changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import sendOrder from "./sendOrder";
-import DishesForm from "./components/DishesForm.js";
+import DishesForm from "./components/DishesForm/DishesForm";
 import store from "./store";
 
 ReactDOM.render(
