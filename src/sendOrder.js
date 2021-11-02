e5e107b0c9dd25f4e2301d58d52ee4cce149f17f
Mateusz Przęczek <53494694+mateuszprzeczek@users.noreply.github.com>
2021-11-02 11:15:32 +0100

    validation for send request was added

--- src/sendOrder.js
+++ src/sendOrder.js
@@ -1,7 +1,30 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+const URL = 'https://frosty-wood-6558.getsandbox.com:443/dishes';
 
 export default (async function showResults(values) {
-  await sleep(500); // simulate server latency
-  window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+  await sleep(500);
+  let result = {};
+  if(values.type === 'pizza') {
+    result = {name:values.name, preparation_time: values.preparation_time, type: values.type, no_of_slices:parseInt(values.no_of_slices), diameter:parseInt(values.diameter)}
+  } else if(values.type === 'soup') {
+    result = {name:values.name, preparation_time:values.preparation_time, type:values.type, spiciness_scale:parseInt(values.spiciness_scale)}
+  } else if(values.type === 'sandwith') {
+    result = {name:values.name, preparation_time:values.preparation_time, type:values.type, slices_of_bread:parseInt(values.slices_of_bread)}
+  }
+  const requestOptions = {
+    method: 'POST',
+    body: JSON.stringify(result, null, 2),
+    headers: {
+       'Content-Type': 'application/json'
+    }
+ }
+ try {
+  const res = await fetch(URL, requestOptions, {mode: 'cors'});
+  const data = await res.json();
+  console.log("data: ",data);
+} catch (error) {
+   window.alert(error);
+}
+  window.alert(`You submitted:\n\n${JSON.stringify(result, null, 2)}`);
 });
